64c8836291fdece24fa2b330fd3c29df92500fca
ANDREY <41635263+FreylGit@users.noreply.github.com>
2023-03-25 02:51:14 +0300

    Добавьте файлы проекта.

---
+++ PokemonReviewApp/Data/Interfases/IPokemonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data.Interfases
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokimonRaiting(int pokeId);
+        bool PokemonExists(int id);
+    }
+}
---
+++ PokemonReviewApp/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Data.Interfases;
+using PokemonReviewApp.Data.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddTransient<SeedData>();
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    SeedData(app);
+
+}
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedData>();
+        service.SeedDataContext();
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
